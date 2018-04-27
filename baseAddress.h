3036a12ab974696760b60ec4934bf6a4d5e99ce0
ouzian <judgeou@vip.qq.com>
2018-04-27 20:02:59 +0800

    更新基址

--- baseAddress.h
+++ baseAddress.h
@@ -1,13 +1,13 @@
 #define ADDRESS int
 #define LENGTH(x) (sizeof(x) / sizeof((x)[0]))
 
-#define 人物基址 0x04DCD598
-#define 阵营偏移 0x828
+#define 人物基址 0x04E004D8
+#define 阵营偏移 0x82C
 #define 类型偏移 0x98
-#define CALL_扣血 0x01EFDCE0
+#define CALL_扣血 0x01F13760
 
-#define PLAYER_ADDR 0x4DCD598
-#define HOOK_LOOP 0x02297DFE
-#define SSS_ADDR 0x4CD511C
+#define PLAYER_ADDR 0x04E004D8
+#define HOOK_LOOP 0x022AFACE
+#define SSS_ADDR 0x04D07C84
 #define ADDR_PICKUP1 0x23159f7
 #define ADDR_PICKUP2 0x23159c6
